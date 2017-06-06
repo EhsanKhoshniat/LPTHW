bdc8d3652f7e0bc143ed8c7fccb3f3c02736def8
Ehsan <ehsan.k93@gmail.com>
2017-06-07 01:43:22 +0430

    Add comments for ex6

--- ex6.py
+++ ex6.py
@@ -6,11 +6,11 @@ y = "Those who know %s and those who %s." % (binary, do_not)
 print x 
 print y
 
-print "I said: %r." % x
-print "I also said: %s." % y
+print "I said: %r." % x # %r is used for debugging since it displays the "raw" data of the variable.
+print "I also said: %s." % y # use %s and others for displaying to users.
 
 hilarious = False
-joke_evaluation = "Isn't that joke so funny?! %r"
+joke_evaluation = "Isn't that joke so funny?! %r" #No need to define format in same line. BTW I find it funny.
 
 print joke_evaluation % hilarious
 
