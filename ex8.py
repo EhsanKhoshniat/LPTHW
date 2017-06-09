b5b284f8953dc4f7383e7288903d9b71188543bd
Ehsan <ehsan.k93@gmail.com>
2017-06-09 19:23:14 +0430

    Fix error on ex8 and add a comment.

--- ex8.py
+++ ex8.py
@@ -4,8 +4,8 @@ print formatter % ("one", "two", "three", "four")
 print formatter % (True, False, False, True)
 print formatter % (formatter, formatter, formatter, formatter)
 print formatter % (
-	"I had this thing",
-	"That you could type up right.",
+	"I had this thing", #I forgot the "," and got "not enough arguments for format string" error.
+	"That you could type up right.", 
 	"But it didn't sing",
 	"So I said goodnight."
 	)
