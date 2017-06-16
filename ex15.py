d8aa81767c5e657f9349b0356bc4caa0d40387ba
Ehsan <ehsan.k93@gmail.com>
2017-06-16 15:39:16 +0430

    comment some lines

--- ex15.py
+++ ex15.py
@@ -1,15 +1,15 @@
 from sys import argv 
 
-script , filename = argv
+script , filename = argv #define command line arguments
 
-txt = open(filename)
+txt = open(filename)	#opens file defined by command-line
 
-print "Here's your file %r:" % filename
-print txt.read()
+print "Here's your file %r:" % filename 	#prints the opened file's name
+print txt.readlines()	#reads the file's content
 
-print "Type the filename again:"
-file_agian = raw_input("> ")
+print "Type the filename again:"	
+file_agian = raw_input("> ")	#open file in another way, with raw_input method 
 
-txt_again = open(file_agian)
+txt_again = open(file_agian)	#opens file defined by raw_input
 
-print txt_again.read()
+print txt_again.read()			#read the file
