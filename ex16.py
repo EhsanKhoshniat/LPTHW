ccfd212021a264e1377e7f023042234a61b895f4
Ehsan <ehsan.k93@gmail.com>
2017-06-16 17:00:44 +0430

    fix repetitions on writing line and add read commant.

--- ex16.py
+++ ex16.py
@@ -1,12 +1,12 @@
 from sys import argv
 
-script, filename = argv
+script, filename = argv #define command-line arguments
 
-print "We're going to erase %r." % filename
+print "We're going to erase %r." % filename #tells something about the file defined by command-line
 print "If you don't whant that, hit CTRL-C (^C)."
 print "If you do whant that, hit RETURN."
 
-raw_input("?")
+raw_input("?")	#CTRL-C interrupts the command-line
 
 print "Opening the file..."
 target = open(filename, 'w')
@@ -22,12 +22,20 @@ line3 = raw_input("line 3: ")
 
 print "I'm going to write these to the file."
 
-target.write(line1)
-target.write("\n")
-target.write(line2)
-target.write("\n")
-target.write(line3)
-target.write("\n")
+final_txt = line1 + "\n" + line2 + "\n" + line3
+target.write(final_txt)
+# target.write(line1)
+# target.write("\n")
+# target.write(line2)
+# target.write("\n")
+# target.write(line3)
+# target.write("\n")
+
+
 
 print "And finally, we close it."
 target.close()
+
+new_file = open(filename)
+print "Here is the new file: "
+print new_file.read()
