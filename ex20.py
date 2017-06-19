6e25045f68fbe2624f1786c9a1b4e77ae68c4676
Ehsan <ehsan.k93@gmail.com>
2017-06-20 02:06:51 +0430

    Add some comments to exercise 20.

--- ex20.py
+++ ex20.py
@@ -3,13 +3,14 @@ from sys import argv
 script, input_file = argv
 
 def print_all(f):
-	print f.read()	
+	print f.read()	#well, reads the file
 
 def rewind(f):
-	f.seek(0)
+	f.seek(0)	# A seek() operation moves pointer to some other part of the file,
+	# so you can read or write at that place
 
 def print_a_line(line_count, f):
-	print line_count, f.readline()
+	print line_count, f.readline() #prints line's count and a line of file
 
 current_file = open(input_file)
 
@@ -19,7 +20,7 @@ print_all(current_file)
 
 print "Now let's rewind, kind of like a tape."
 
-rewind(current_file)
+rewind(current_file) #move pointer to the the beginning of the file
 
 print "Let's print three lines:"
 
