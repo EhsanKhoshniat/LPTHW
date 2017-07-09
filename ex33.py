6f7abda0ffad730696aa0a837182d4b467d1973a
Ehsan <ehsan.k93@gmail.com>
2017-07-10 00:54:59 +0430

    Creat function for exercise 33

--- ex33.py
+++ ex33.py
@@ -1,17 +1,27 @@
-i = 0
-numbers = []
+# i = 0
+# numbers = []
 
-while i < 6:
-	print "At te top i is %d" % i
-	numbers.append(i)
+# while i < 6:
+# 	print "At te top i is %d" % i
+# 	numbers.append(i)
 
-	i += 1
-	print "Numbers now: ", numbers
-	print "At the bottum i is %d" % i
+# 	i += 1
+# 	print "Numbers now: ", numbers
+# 	print "At the bottum i is %d" % i
 
 
-print "The numbers: "
+# print "The numbers: "
 
-for num in numbers:
-	print num
+# for num in numbers:
+# 	print num
 
+def printnum(n, x):
+	i = 0
+	numbers = []
+	while i < n:
+		numbers.append(i)
+		i += x
+	for num in numbers:
+		print num
+
+printnum(6, 2)
