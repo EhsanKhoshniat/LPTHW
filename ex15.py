536a8a4aa618a8686c16f1b2cf2fdc81f554b457
Ehsan <ehsan.k93@gmail.com>
2017-07-13 12:26:36 +0430

    Used with statement on line 13

--- ex15.py
+++ ex15.py
@@ -8,10 +8,13 @@ print "Here's your file %r:" % filename 	#prints the opened file's name
 print txt.readlines()	#reads the file's content
 
 print "Type the filename again:"	
-file_agian = raw_input("> ")	#open file in another way, with raw_input method 
+file_again = raw_input("> ")	#open file in another way, with raw_input method 
 
-txt_again = open(file_agian)	#opens file defined by raw_input
+with open(file_again) as txt_again:  # "with" statement 
+	print txt_again.read()
 
-print txt_again.read()			#read the file
+# txt_again = open(file_agian)	#opens file defined by raw_input
+
+# print txt_again.read()			#read the file
 
 txt_again.close()
